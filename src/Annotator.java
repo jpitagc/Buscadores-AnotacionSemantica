184808d2114256cd144a9e166d928578c6ec4c28
julio <julio@10.118.44.103>
2022-12-14 20:49:00 +0100

    Correcciones Annotator

--- src/Annotator.java
+++ src/Annotator.java
@@ -1,22 +1,27 @@
-package src;
 
 
-import es.uc3m.miaa.utils.*;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Vector;
+
+import es.uc3m.miaa.utils.Entity;
+import es.uc3m.miaa.utils.MyGATE;
 
 
 
 public class Annotator {
 
-	public ArrayList<String> webpages = new Arraylist<String>();
+	public static ArrayList<String> webpages = new ArrayList<String>();
 
     public static void main(String[] args) {
 
     	BufferedReader reader;
         System.out.println("Running annotator command...");
         try{
-        	if (args.length()<1 || args[0]==null){
+        	if (args.length<1 || args[0]==null){
         		System.out.println("Missing file argument"); 
         		System.exit(-1);
         		}
@@ -31,26 +36,26 @@ public class Annotator {
 
 			reader.close();
         } catch(Exception e){
-            System.out.println("Error reading file: "+e.printStackTrace());
+            System.out.println("Error reading file: "+e);
         }
-        String defaultClass= args.length()>1 && args[1]!=null?args[1]:"#webPage";
-        MyGate myGate = MyGate.getInstance();
-        File myFile = new File("test.ttl");
+        String defaultClass= args.length>1 && args[1]!=null?args[1]:"#webPage";
+        MyGATE myGate = MyGATE.getInstance();
+        File myFile = new File(args[0].replace(".txt",".ttl"));
         myFile.createNewFile();
         FileWriter myWriter = new FileWriter(myFile);
     	ArrayList<String> types = new ArrayList<String>();
         for(int i=0; i < webpages.size(); i++ ) {
-        	myWriter.write("?"+webpages[i]+" a "defaultClass+" .\n")
-        	ArrayList<Entity> entities = myGate.findEntities(webpages[i]);
-        	for(int i=0; i < entities.size(); i++ ) {
-        		myWriter.write("?"+entities[i].text+" a <#"entities[i].type+"> .\n");
-        		if(!types.contains(entities[i].type)) {
-        			types.add(entities[i].type);
+        	myWriter.write("<"+webpages.get(i)+"> a "+defaultClass+" .\n");
+        	ArrayList<Entity> entities = myGate.findEntities(webpages.get(i));
+        	for(int j=0; j < entities.size(); j++ ) {
+        		myWriter.write("<"+entities.get(j).getText()+"> a "+entities.get(j).getType()+" .\n");
+        		if(!types.contains(entities.get(j).getType())) {
+        			types.add(entities.get(j).getType());
         		}
         	}
         }
         for(int i = 0; i < types.size();i++) {
-        	myWriter.write("?"+types[i]+" rdfs:subPropertyOf <#Entity>+" ; <#named> \""+types[i]+"\" . \n")
+        	myWriter.write("<"+types.get(i)+"> rdfs:subPropertyOf <#Entity>"+" ; <#named> \""+types.get(i)+"\" . \n");
         }
         
     }
